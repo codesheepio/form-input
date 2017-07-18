9100ca278aea841c66ad35cb285ae52176772559
Ake Tangkananond <iamake@gmail.com>
2017-07-18 18:18:08 +0700

    HTML Input

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -1,15 +1,38 @@
 import React from 'react';
 
-function SimpleForm() {
-  return (
-    <div className="container">
-      <form>
-        Name:
-        <input type="text" name="name" />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
+class SimpleForm extends React.Component {
+  constructor() {
+    super();
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(`submitted: ${ this.state.value }`);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={ this.handleSubmit }>
+          Name:
+          <input
+            type="text"
+            value={ this.state.value }
+            onChange={ this.handleChange }
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SimpleForm;
