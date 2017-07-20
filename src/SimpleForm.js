bdaf67dac352bb62982c8978a4b45c8a773ee764
Ake Tangkananond <iamake@gmail.com>
2017-07-20 18:20:10 +0700

    Citizen id form input

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -3,7 +3,12 @@ import React from 'react';
 class SimpleForm extends React.Component {
   constructor() {
     super();
-    this.state = { name: '', biblio: '' };
+
+    this.state = {
+      name: '',
+      citizenid: '',
+      biblio: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,7 +18,27 @@ class SimpleForm extends React.Component {
     const name = e.target.name;
     const value = e.target.value;
 
-    this.setState({ [name]: value })
+    switch (name) {
+      case 'citizenid':
+        const numbers = value.replace(/[^\d]/g, '');
+        if (numbers.length > 13)
+          break;
+
+        const groups = /(\d{0,1})(\d{0,4})(\d{0,5})(\d{0,2})(\d{0,1})/.exec(numbers);
+        const citizenid = groups.slice(1).reduce(
+            (result, group) => result.length > 0 && group.length > 0
+              ? `${result} ${group}`
+              : `${result}${group}`,
+            ''
+          );
+
+        this.setState({ citizenid })
+        break;
+
+      default:
+        this.setState({ [name]: value })
+        break;
+    }
   }
 
   handleSubmit(e) {
@@ -29,6 +54,12 @@ class SimpleForm extends React.Component {
           <input
             name="name"
             value={ this.state.name }
+            onChange={ this.handleChange }
+          />
+          <h2>Citizen ID:</h2>
+          <input
+            name="citizenid"
+            value={ this.state.citizenid }
             onChange={ this.handleChange }
           />
           <h2>Bibliography:</h2>
