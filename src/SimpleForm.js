2127b5618daebee1d8a3bf2874d72a1c32c81e8e
Ake Tangkananond <iamake@gmail.com>
2017-07-20 17:20:34 +0700

    Multiple inputs

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -3,28 +3,38 @@ import React from 'react';
 class SimpleForm extends React.Component {
   constructor() {
     super();
-    this.state = { value: '' };
+    this.state = { name: '', biblio: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({ value: e.target.value })
+    const name = e.target.name;
+    const value = e.target.value;
+
+    this.setState({ [name]: value })
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log(`submitted: ${ this.state.value }`);
+    console.log('submitted:', this.state);
   }
 
   render() {
     return (
       <div className="container">
         <form onSubmit={ this.handleSubmit }>
+          <h2>Name:</h2>
+          <input
+            name="name"
+            value={ this.state.name }
+            onChange={ this.handleChange }
+          />
           <h2>Bibliography:</h2>
           <textarea
-            value={ this.state.value }
+            name="biblio"
+            value={ this.state.biblio }
             onChange={ this.handleChange }
             rows={5}
             style={{width: '100%'}}
